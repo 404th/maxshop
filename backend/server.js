891fa063092d9a9e4ee3fee64384308d44de071a
= <=>
2021-04-15 05:39:58 +0500

    SOME BASIC

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv").config({ path:"./.env" })
+// mongoose.Promise = global.Promise
 
 // middlewares
 app.use( cors() )
@@ -9,12 +11,27 @@ app.use( express.json() )
 // !!! middlewares
 
 // connecting to MongoDB
-mongoose.connection(  )
+async function startServer(){
+  const MONGO_URI = process.env.MONGO_URI
+  await mongoose.connect( MONGO_URI, {
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+  }, err => {
+    if(err) {
+      console.log("Failed: DB is not connected!")
+    } else {
+      console.log("Success:DB is connected successfully!")
+    }
+  } )
+}
+startServer()
+// !!! connecting to MongoDB
 
 // initializing routes
 
 // !!! initializing routes
-
 // listening PORT
 const PORT = process.env.PORT || 8181
 app.listen( PORT, () => console.log(`Server is running on PORT:${ PORT }`) )
