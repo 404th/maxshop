1a10bc7e3cd58b8ef4dadb213c8509cc2a9e2328
= <=>
2021-04-16 21:47:16 +0500

    Footer is ready

--- client/src/Components/Footer/footer.jsx
+++ client/src/Components/Footer/footer.jsx
@@ -1,7 +1,10 @@
 import "./style/footerStyle.css"
 import { Link } from 'react-router-dom'
 
+
 function Footer () {
+  let year_footer = new Date().getFullYear()
+
   return (
     <div className="footer_container">
       <div className="footer_container_cover">
@@ -13,7 +16,56 @@ function Footer () {
           </p>
           <Link className="footer_container_cover_ad_p_link no-select no-drag flex-centering-item" to="/">LEARN MORE</Link>
         </div>
-        <div className="footer_container_cover_navigator"></div>
+        <div className="footer_container_cover_navigator">
+          <div className="footer_container_cover_navigator_group">
+            <div className="footer_container_cover_navigator_group_title">
+              <span className="footer_container_cover_navigator_group_title_span">Customer Service</span>
+            </div>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">About us</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Delivery Information</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Privacy Policy</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Terms & Policy</Link>
+          </div>
+          <div className="footer_container_cover_navigator_group">
+            <div className="footer_container_cover_navigator_group_title">
+              <span className="footer_container_cover_navigator_group_title_span">Customer Service</span>
+            </div>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">About us</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Delivery Information</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Privacy Policy</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Terms & Policy</Link>
+          </div>
+          <div className="footer_container_cover_navigator_group">
+            <div className="footer_container_cover_navigator_group_title">
+              <span className="footer_container_cover_navigator_group_title_span">Customer Service</span>
+            </div>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">About us</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Delivery Information</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Delivery Information</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Delivery Information</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Delivery Information</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Privacy Policy</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Terms & Policy</Link>
+          </div>
+          <div className="footer_container_cover_navigator_group">
+            <div className="footer_container_cover_navigator_group_title">
+              <span className="footer_container_cover_navigator_group_title_span">Customer Service</span>
+            </div>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">About us</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Delivery Information</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Privacy Policy</Link>
+            <Link className="footer_container_cover_navigator_group_link no-drag" to="/">Terms & Policy</Link>
+          </div>
+        </div>
+        <div className="footer_container_cover_social">
+          <span className="footer_container_cover_social_title">&copy; { year_footer } Onecom Design, All right reserved!</span>
+          <div className="footer_container_cover_social_icons">
+            <Link to={"/"} className="fa fa-facebook"></Link>
+            <Link to={"/"} className="fa fa-twitter"></Link>
+            <Link to={"/"} className="fa fa-instagram"></Link>
+            <Link to={"/"} className="fa fa-linkedin"></Link>
+          </div>
+        </div>
       </div>
     </div>
   )
